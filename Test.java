ff5cf5d1c45d5d97d59ee95c758f9227dd62779a
TheodoreTsimiklis <theodoretsimiklis@gmail.com>
2020-12-01 00:23:19 -0500

    First commit, this is the student project, we worked on in class, using OOP

---
+++ Test.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+import clock.Clock;
+
+/**
+ *
+ * @author 15148
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*
+        Student stu1 = new Student("Theo", 17, "male", new Address("123", "35"
+                , "Main Street", "Montreal", "QC", "A1A 1A1"));
+        System.out.println(stu1);
+        
+        //stu1.setAddress(new Address("2", "12", "Second Street", "Sherbrooke", "QC", "B2B 3B2"));
+        //System.out.println(stu1);
+        
+        stu1.getAddress().setAptNum("223");
+        stu1.getAddress().setStreetNum("122");
+        stu1.getAddress().setZipcode("b2b 2b2");
+        System.out.println(stu1);
+        stu1.getAddress().setCity("Toronto");
+        stu1.getAddress().setProvince("ON");
+        stu1.checkDistance();
+        */
+        /*
+        Clock c = new Clock(14, 10, 6);
+        Clock c2 = new Clock(c);
+        
+        c.setHr(0);
+        System.out.println(c2);
+        */
+        
+        
+        
+    }
+}
